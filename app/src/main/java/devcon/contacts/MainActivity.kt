effcfb24d5b5d63df7e15ccb25ecff4905eaff63
hoon92 <hoon92@hoon92s-MacBook-Air.local>
2025-03-13 23:46:50 +0900

    feat : 생일 클릭 이벤트  구현, 성별 선택안되도록 수정

--- app/src/main/java/devcon/contacts/MainActivity.kt
+++ app/src/main/java/devcon/contacts/MainActivity.kt
@@ -1,6 +1,8 @@
 package devcon.contacts
 
+import android.app.DatePickerDialog
 import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -24,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var groupMoreBtn: Group
     private lateinit var groupMoreView: Group
     private lateinit var viewMore: View
-
+    private lateinit var etBirthday: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         groupMoreBtn = findViewById(R.id.group_more_btn)
         groupMoreView = findViewById(R.id.group_more_view)
         viewMore = findViewById(R.id.view_more)
+        etBirthday = findViewById(R.id.et_birthday)
 
 
         btnSave.setOnClickListener {
@@ -60,6 +63,11 @@ class MainActivity : AppCompatActivity() {
             groupMoreBtn.visibility = View.GONE
             groupMoreView.visibility = View.VISIBLE
         }
+
+        etBirthday.setOnClickListener {
+            showDatePickerDialog()
+        }
+
     }
 
     private fun isNameEmpty(): Boolean {
@@ -79,5 +87,18 @@ class MainActivity : AppCompatActivity() {
 
         focusEditText.requestFocus()
         inputMethodManager.showSoftInput(focusEditText, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+            val result = "${selectedYear}.${selectedMonth}.${selectedDay}"
+            etBirthday.setText(result)
+        }, year, month, day).show()
+
     }
 }
