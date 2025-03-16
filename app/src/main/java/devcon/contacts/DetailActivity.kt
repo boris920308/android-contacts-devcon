9046b80be921f74821f41ee12190ab491a834136
boris920308 <jhoonj0308@gmail.com>
2025-03-16 18:52:38 +0900

    feat : DetailActivity View 구현

---
+++ app/src/main/java/devcon/contacts/DetailActivity.kt
@@ -0,0 +1,15 @@
+package devcon.contacts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import devcon.learn.contacts.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
